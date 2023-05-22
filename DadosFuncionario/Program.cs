99129d1a62a4372a4805fc24d0cdbd9b582300cb
joaopedronqt <129910247+joaopedronqt@users.noreply.github.com>
2023-05-22 17:51:31 -0300

    Add files via upload

---
+++ DadosFuncionario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace DadosFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Funcionario fun = new Funcionario();
+
+            System.Console.Write("Nome: ");
+            fun.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            fun.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            fun.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            System.Console.WriteLine();
+            Console.Write("Funcionario: " + fun);
+
+            System.Console.WriteLine();
+            
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double por = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            fun.AumentarSalario(por);
+            
+            System.Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + fun);
+
+        }           
+    }
+}
